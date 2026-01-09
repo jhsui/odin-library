04f146aafbecdfe9ebe6f1c38b106d6f2b29e46d
jhsui <jiahansui1998@gmail.com>
2026-01-09 15:48:39 -0500

    add empty field validation and custom message.

--- script-by-class.js
+++ script-by-class.js
@@ -7,7 +7,12 @@ class Book {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    // this.read = read;
+    if (read === "true" || read === true) {
+      this.read = true;
+    } else {
+      this.read = false;
+    }
 
     this.info = function () {
       let readOrNot = "";
@@ -27,19 +32,7 @@ class Book {
       );
     };
   }
-
-  // if (!new.target) {
-  //   throw Error("You must use the 'new' operator to call the constructor.");
-  // }
-
-  // this.id = crypto.randomUUID();
-  // this.title = title;
-  // this.author = author;
-  // this.pages = pages;
-  // this.read = read === true || read === "true";
 }
-
-const book1 = new Book("t", "s", 1, "not read");
 
 function addBookToLibrary(title, author, pages, read) {
   let book = new Book(title, author, pages, read);
@@ -51,8 +44,6 @@ myLibrary.push(new Book("BookOne", "AuthorOne", 10, true));
 myLibrary.push(new Book("BookTwo", "AuthorTwo", 20, false));
 myLibrary.push(new Book("BookThree", "AuthorThree", 30, false));
 
-// console.log(myLibrary);
-
 showButton.onclick = function displayAllBooks() {
   const container = document.querySelector(".container");
 
@@ -60,7 +51,7 @@ showButton.onclick = function displayAllBooks() {
 
   for (const book of myLibrary) {
     const bookContainer = document.createElement("div");
-    // bookContainer.dataset.id = book.id;
+
     const article = document.createElement("article");
 
     article.textContent = book.info();
@@ -68,8 +59,6 @@ showButton.onclick = function displayAllBooks() {
     const removeBookButton = document.createElement("button");
     removeBookButton.textContent = "Remove";
     removeBookButton.onclick = function () {
-      // myLibrary = myLibrary.filter((obj) => obj.id !== book.id);
-
       const index = myLibrary.findIndex((obj) => obj.id === book.id);
       if (index !== -1) {
         myLibrary.splice(index, 1);
@@ -92,22 +81,70 @@ showButton.onclick = function displayAllBooks() {
   }
 };
 
-const submitButton = document.getElementById("form-submit-button");
-submitButton.onclick = function () {
-  addBookToLibrary(
-    document.getElementById("title").value,
-    document.getElementById("author").value,
-    document.getElementById("pages").value,
-    document.getElementById("read").value
-  );
+const form = document.querySelector("form");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const read = document.getElementById("read");
 
-  showButton.click();
-  formContainer.classList.remove("show-form");
-  document.querySelector("form").reset();
+const validation = (e) => {
+  e.preventDefault();
+
+  if (title.validity.valueMissing) {
+    title.setCustomValidity("Title required!!!");
+  } else {
+    title.setCustomValidity("");
+  }
+
+  // title.reportValidity();
+
+  if (author.validity.valueMissing) {
+    author.setCustomValidity("author required!!!");
+  } else {
+    author.setCustomValidity("");
+  }
+
+  // author.reportValidity();
+
+  if (pages.validity.valueMissing) {
+    pages.setCustomValidity("pages required!!!");
+  } else {
+    pages.setCustomValidity("");
+  }
+
+  // pages.reportValidity();
+
+  if (read.validity.valueMissing) {
+    read.setCustomValidity("read required!!!");
+  } else {
+    read.setCustomValidity("");
+  }
+
+  // read.reportValidity();
+};
+
+const submitButton = document.getElementById("form-submit-button");
+submitButton.onclick = function (e) {
+  validation(e);
+
+  if (form.checkValidity()) {
+    addBookToLibrary(
+      document.getElementById("title").value,
+      document.getElementById("author").value,
+      document.getElementById("pages").value,
+      document.getElementById("read").value
+    );
+
+    showButton.click();
+    formContainer.classList.remove("show-form");
+    document.querySelector("form").reset();
+  } else {
+    form.reportValidity();
+  }
 };
 
 const formContainer = document.querySelector(".form-container");
-// const formContainer = document.getElementsById("form-container");
+
 const newBookButton = document.getElementById("new-book-btn");
 
 newBookButton.addEventListener("click", () => {
